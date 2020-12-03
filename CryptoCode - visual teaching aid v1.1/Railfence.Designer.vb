092ee98141002b655ac72ce07045e04fa99061be
luke1hl <51419088+luke1hl@users.noreply.github.com>
2020-12-03 15:42:17 +0000

    Update Railfence.Designer.vb

--- CryptoCode - visual teaching aid v1.1/Railfence.Designer.vb
+++ CryptoCode - visual teaching aid v1.1/Railfence.Designer.vb
@@ -34,6 +34,7 @@ Partial Class Railfence
         Me.lettereight = New System.Windows.Forms.TextBox()
         Me.TrackBar1 = New System.Windows.Forms.TrackBar()
         Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
         CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -156,11 +157,23 @@ Partial Class Railfence
         Me.Label1.Size = New System.Drawing.Size(0, 17)
         Me.Label1.TabIndex = 10
         '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 28.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(12, 377)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 61)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "X"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
         'Railfence
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TrackBar1)
         Me.Controls.Add(Me.lettereight)
@@ -196,4 +209,5 @@ Partial Class Railfence
     Friend WithEvents lettereight As TextBox
     Friend WithEvents TrackBar1 As TrackBar
     Friend WithEvents Label1 As Label
+    Friend WithEvents Button2 As Button
 End Class
