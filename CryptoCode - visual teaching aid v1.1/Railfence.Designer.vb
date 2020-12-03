5dfdd95800c8ecd66c80c15c55d54df05ed64bec
luke1hl <51419088+luke1hl@users.noreply.github.com>
2020-12-03 15:20:39 +0000

    Update Railfence.Designer.vb

--- CryptoCode - visual teaching aid v1.1/Railfence.Designer.vb
+++ CryptoCode - visual teaching aid v1.1/Railfence.Designer.vb
@@ -33,6 +33,7 @@ Partial Class Railfence
         Me.letterseven = New System.Windows.Forms.TextBox()
         Me.lettereight = New System.Windows.Forms.TextBox()
         Me.TrackBar1 = New System.Windows.Forms.TrackBar()
+        Me.Label1 = New System.Windows.Forms.Label()
         CType(Me.TrackBar1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -146,11 +147,21 @@ Partial Class Railfence
         Me.TrackBar1.TabIndex = 9
         Me.TrackBar1.Value = 2
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.White
+        Me.Label1.Location = New System.Drawing.Point(345, 252)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(0, 17)
+        Me.Label1.TabIndex = 10
+        '
         'Railfence
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TrackBar1)
         Me.Controls.Add(Me.lettereight)
         Me.Controls.Add(Me.letterseven)
@@ -184,4 +195,5 @@ Partial Class Railfence
     Friend WithEvents letterseven As TextBox
     Friend WithEvents lettereight As TextBox
     Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents Label1 As Label
 End Class
